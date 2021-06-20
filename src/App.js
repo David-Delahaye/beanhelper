f80ce8ef77d4a66e3a624511e36154b5eb836d72
David Delahaye <delahaye3@hotmail.co.uk>
2021-06-20 16:26:26 +0100

    fix blogs array error

--- src/App.js
+++ src/App.js
@@ -9,27 +9,29 @@ function App() {
     const response = db.collection("Blogs");
     response.get().then((data) => {
       data.docs.forEach((item) => {
-        setBlogs(item.data());
+        setBlogs([item.data()]);
       });
     });
   }, []);
 
+  if (blogs.length === 0) {
+    return "HEY";
+  }
+
   return (
     <div className="App">
-      {blogs &&
-        blogs.length !== 0 &&
-        blogs.map((blog) => {
-          return (
-            <div
-              className="blog-container"
-              style={{ width: 500 + "px", textAlign: "center" }}
-            >
-              <h4>{blog.title}</h4>
-              <h5>{blog.posted_by}</h5>
-              <p>{blog.body}</p>
-            </div>
-          );
-        })}
+      {blogs.map((blog) => {
+        return (
+          <div
+            className="blog-container"
+            style={{ width: 500 + "px", textAlign: "center" }}
+          >
+            <h4>{blog.title}</h4>
+            <h5>{blog.posted_by}</h5>
+            <p>{blog.body}</p>
+          </div>
+        );
+      })}
     </div>
   );
 }
