688ba9163dcc610fc0316d902186fd85c642db5c
David Delahaye <delahaye3@hotmail.co.uk>
2021-07-04 20:59:21 +0100

    add bean editor

--- src/App.js
+++ src/App.js
@@ -1,56 +1,111 @@
-import "./App.css";
 import db from "./firebase.config";
 import React, { useState, useEffect } from "react";
 import firebase from "firebase";
+import {
+  ChakraProvider,
+  Flex,
+  Divider,
+  Box,
+  Center,
+  Slider,
+  SliderTrack,
+  SliderFilledTrack,
+  SliderThumb,
+} from "@chakra-ui/react";
+import ColourPicker from "./Components/ColourPicker";
+
+const sampleBean = { height: 200, colour: "#FF0000", width: 100 };
 
 function App() {
-  const [blogs, setBlogs] = useState([]);
-  const [number, setNumber] = useState(0);
+  const [currentBean, setCurrentBean] = useState();
+  const [creatorBean, setCreatorBean] = useState(sampleBean);
 
-  const setRandomNumber = () => {
-    const randomNumber = firebase.functions().httpsCallable("randomNumber");
-    randomNumber().then((randomNum) => {
-      console.log(randomNum.data);
-      setNumber(randomNum.data);
-    });
+  // const [number, setNumber] = useState(0);
+
+  // const setRandomNumber = () => {
+  //   const randomNumber = firebase.functions().httpsCallable("randomNumber");
+  //   randomNumber().then((randomNum) => {
+  //     console.log(randomNum.data);
+  //     setNumber(randomNum.data);
+  //   });
+  // };
+
+  const handleChange = (newColour) => {
+    setCreatorBean({ ...creatorBean, colour: newColour });
   };
 
-  useEffect(() => {
-    const response = db.collection("Blogs");
-    response.get().then((data) => {
-      data.docs.forEach((item) => {
-        setBlogs([item.data()]);
-      });
-    });
+  useEffect(async () => {
+    const beanRef = db.collection("Beans").doc("bqi9s2KigtR1QbUgsiS1");
+    const bean = await beanRef.get();
+    if (!bean.exists) {
+      console.log("No such document!");
+    } else {
+      setCurrentBean(bean.data());
+    }
   }, []);
 
-  if (blogs.length === 0) {
-    return "HEY";
+  console.log(currentBean);
+
+  if (currentBean === undefined) {
+    return "";
   }
 
   return (
-    <div className="App">
-      <h2>{number}</h2>
-      <button
-        onClick={() => {
-          setRandomNumber();
-        }}
-      >
-        generate a random number
-      </button>
-      {blogs.map((blog) => {
-        return (
-          <div
-            className="blog-container"
-            style={{ width: 500 + "px", textAlign: "center" }}
-          >
-            <h4>{blog.title}</h4>
-            <h5>{blog.posted_by}</h5>
-            <p>{blog.body}</p>
-          </div>
-        );
-      })}
-    </div>
+    <ChakraProvider resetCSS>
+      <div className="App">
+        <Flex
+          justifyContent="center"
+          alignItems="center"
+          width="100vw"
+          height="100vh"
+        >
+          <Center w="300px" h="300px">
+            <div
+              className="bean"
+              style={{
+                background: creatorBean.colour,
+                width: creatorBean.width,
+                height: creatorBean.height,
+                borderRadius: "500px",
+              }}
+            ></div>
+            <Slider
+              aria-label="slider-ex-3"
+              value={creatorBean.height}
+              onChange={(e) => {
+                setCreatorBean({ ...creatorBean, height: e });
+              }}
+              orientation="vertical"
+              minH="32"
+            >
+              <SliderTrack>
+                <SliderFilledTrack />
+              </SliderTrack>
+              <SliderThumb />
+            </Slider>
+
+            <Slider
+              aria-label="slider-ex-3"
+              value={creatorBean.width}
+              onChange={(e) => {
+                setCreatorBean({ ...creatorBean, width: e });
+              }}
+              orientation="vertical"
+              minH="32"
+            >
+              <SliderTrack>
+                <SliderFilledTrack />
+              </SliderTrack>
+              <SliderThumb />
+            </Slider>
+            <ColourPicker
+              defaultColor={creatorBean.colour}
+              handleChange={handleChange}
+            />
+          </Center>
+        </Flex>
+      </div>
+    </ChakraProvider>
   );
 }
 
---
+++ src/Components/ColourPicker.js
@@ -0,0 +1,90 @@
+import React, { useEffect, useState } from "react";
+import {
+  Popover,
+  PopoverTrigger,
+  Button,
+  PopoverContent,
+  PopoverArrow,
+  PopoverCloseButton,
+  PopoverHeader,
+  PopoverBody,
+  Center,
+  SimpleGrid,
+  Input,
+  Heading,
+} from "@chakra-ui/react";
+
+export default function ColourPicker({ defaultColor, handleChange }) {
+  console.log(defaultColor);
+  const [color, setColor] = useState(defaultColor);
+  const colors = ["#ff0000", "#00ff00", "#0000ff"];
+
+  useEffect(() => {
+    console.log(color);
+    handleChange(color);
+  }, [color]);
+
+  return (
+    <>
+      <Center marginTop={50}></Center>
+      <Center marginTop={5}>
+        <Popover variant="picker">
+          <PopoverTrigger>
+            <Button
+              aria-label={color}
+              background={color}
+              height="22px"
+              width="22px"
+              padding={0}
+              minWidth="unset"
+              borderRadius={3}
+            ></Button>
+          </PopoverTrigger>
+          <PopoverContent width="170px">
+            <PopoverArrow bg={color} />
+            <PopoverCloseButton color="white" />
+            <PopoverHeader
+              height="100px"
+              backgroundColor={color}
+              borderTopLeftRadius={5}
+              borderTopRightRadius={5}
+              color="white"
+            >
+              <Center height="100%">{color}</Center>
+            </PopoverHeader>
+            <PopoverBody height="120px">
+              <SimpleGrid columns={5} spacing={2}>
+                {colors.map((c) => (
+                  <Button
+                    key={c}
+                    aria-label={c}
+                    background={c}
+                    height="22px"
+                    width="22px"
+                    padding={0}
+                    minWidth="unset"
+                    borderRadius={3}
+                    _hover={{ background: c }}
+                    onClick={() => {
+                      setColor(c);
+                    }}
+                  ></Button>
+                ))}
+              </SimpleGrid>
+              <Input
+                borderRadius={3}
+                marginTop={3}
+                placeholder="red.100"
+                size="sm"
+                value={color}
+                onChange={(e) => {
+                  setColor(e.target.value);
+                }}
+              />
+            </PopoverBody>
+          </PopoverContent>
+        </Popover>
+      </Center>
+    </>
+  );
+}
